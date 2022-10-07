accd782f2e52dfa671db8c0c9f8d4e453bb5300b
John Micat <jmiclat@ufl.edu>
2022-10-07 01:24:27 -0400

    started navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,29 @@
+import React from "react"
 import "./Navbar.css"
+import instagramSVG from '../icons/instagram.svg'
+import searchSVG from '../icons/magnifying-glass-solid.svg'
+import jiatingLogo from '../images/white-black.png'
 
 export default function Navbar() {
   return (
-    <div className="Navbar">
-        Home About Contact Us
+    <div className="Navbar"> 
+        <div className="navLeft">
+
+          <img className="ig-icon" src={instagramSVG}/> {/*Add ALT source for image*/}
+
+        </div>
+        <div className="navCenter">
+          <ul className="navList">
+            <li className="navListItem">HOME</li>
+            <li className="navListItem">ABOUT</li>
+            <li className="navListItem">CONTACT</li>
+          </ul>
+        </div>
+        
+        <div className="navRight">
+          <img className="jiating-logo" src={jiatingLogo}/> {/*Add ALT source for image */}
+          <img className="search-icon" src={searchSVG}/> {/*Add ALT source for image*/}
+        </div>
     </div>
   )
 }
