a41c18668304113892ab5aa44e6c3fe715a577f1
m1del <jmiclat@ufl.edu>
2022-11-05 16:15:17 -0400

    changed file struc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,32 +5,10 @@ import Home from "./pages/Home";
 import Photoshoots from "./pages/Photoshoots";
 
 function App() {  
-  //Accessing different pages
-  let pageComponent
-  switch(window.location.pathname) {
-    case "/":
-      pageComponent = <Home/>
-      break
-    case "/about":
-      pageComponent = <About/>
-      break
-    case "/contact":
-      pageComponent = <Contact/>
-      break
-    case "/photoshoots":
-      pageComponent = <Photoshoots/>
-      break
-    default:
-      pageComponent = <Home/>
-      break
-  }
   
   return(
     <div className="App">
       <Navbar/>
-      <section className="page-content">
-        {pageComponent}
-      </section>
     </div>
   );
 }
