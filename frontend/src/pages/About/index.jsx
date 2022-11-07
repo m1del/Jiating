0de9bf70539bcc8e07b5470d9df8f770c60ff2c4
m1del <jmiclat@ufl.edu>
2022-11-07 09:49:46 -0500

    added mission statement to about

--- frontend/src/pages/About/index.jsx
+++ frontend/src/pages/About/index.jsx
@@ -4,9 +4,22 @@ import './index.scss'
 function About() {
   return (
     <div id='about-container'>
-      <h1>
-        HELLO FROM ABOUT
-      </h1>
+      <div id='text-container'>
+        <h1>
+          ABOUT
+        </h1>
+        <p className='mission-statement'>
+        JiaTing is an all-inclusive cultural and leadership based organization 
+        that aims to promote traditional/modern lion and dragon dance 
+        through training and performance.
+        </p>
+        <p className='mission-statement'>
+        Originating from the University of Florida's Chinese American Student 
+        Association (CASA), JiaTing seeks to support a greater understanding of 
+        Asian heritage in the Gainesville community and is open to all members 
+        regardless of previous experience or background. 
+        </p>
+      </div>
     </div>
   )
 }
