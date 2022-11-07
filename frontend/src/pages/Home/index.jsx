f68e2b09557cc1f88e70bea84e1d5778beb3fa72
m1del <jmiclat@ufl.edu>
2022-11-07 00:17:22 -0500

    finish homepage

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import titeImage2 from '../../assets/images/dragon-dance.png';
 import titleImage from '../../assets/images/line-of-lions.png';
+import mobileLogo from '../../assets/images/sketchy.png';
 import './index.scss';
 
 function Home() {
@@ -24,15 +25,16 @@ function Home() {
         'We are JIATING.',
         1000,
         ]}
-        speed={50} // Custom Speed from 1-99 - Default Speed: 40
-        style={{ fontSize: '6rem' }}
+        className='type-animation'
+        speed={80} // Custom Speed from 1-99 - Default Speed: 40
+        // style={{ fontSize: '4.3vw' }}
         wrapper="span" // Animation will be rendered as a <span>
         repeat={Infinity} // Repeat this Animation Sequence infinitely
       />
       </div>
       <img id='titleImage2' src={titeImage2} alt='Dragon Dance'/>
       <img id='titleImage' src={titleImage} alt='Line of Lion Dancers'/>
-
+      <img id='mobileLogo' src={mobileLogo} alt='Lion Head'/>
       
     </div>
   )
