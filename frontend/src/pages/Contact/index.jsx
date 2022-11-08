4605713a44aff42c9e0a9df7393c2b56b4a1f87b
m1del <jmiclat@ufl.edu>
2022-11-08 11:46:24 -0500

    fixing deploy job

--- frontend/src/pages/Contact/index.jsx
+++ frontend/src/pages/Contact/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 import './index.scss';
 
 function Contact() {
@@ -13,9 +14,19 @@ function Contact() {
         <h4>
           Connect with us on our social medias!
         </h4>
-        <div id='media-icons'>
-          <FaFacebook/>
-          <FaInstagram/>
+        <div id='social-media'>
+          <a href='https://www.instagram.com/jiating.lion.dragon/' target="_blank" rel="noopener noreferrer"> 
+            <FaFacebook className='media-icon' size={60}/>
+          </a>
+          <a href='https://www.facebook.com/groups/192755019429528' target="_blank" rel="noopener noreferrer"> 
+            <FaInstagram className='media-icon' size={60}/>
+          </a>
+          <HiOutlineMail className='media-icon' size={60}
+          onClick={() => window.location 
+          = 'mailto:jiating.lion.dragon@gmail.com'}/>
+          <button>
+            add linktree
+          </button>
         </div>
       </div>
     </div>
