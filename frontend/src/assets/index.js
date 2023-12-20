f12260bf803cd95e36613d69a75eb012a4630c2f
John Miclat <jmiclat@ufl.edu>
2023-12-19 22:50:19 -0500

    Implement Navbar and some stubs

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,7 @@
+import logo from "./logo.png";
+import menu from './menu-bars.svg';
+import close from './xmark-solid.svg';
+export {
+    close, logo, menu
+};
+
---
+++ frontend/src/pages/Events.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Events() {
+  return (
+    <div>
+      event
+    </div>
+  )
+}
+
+export default Events
