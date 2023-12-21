275925bb455f1185597f8b016901127af79db347
John Miclat <jmiclat@ufl.edu>
2023-12-21 00:19:43 -0500

    Add password hashing

---
+++ backend/utils/password.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword creates a bcrypt hash of the passed in password
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+// CheckPasswordHash compares a bcrypt hash with its possible plaintext equivalent
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
