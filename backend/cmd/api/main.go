f31e3a6842cc7d3bc483cf82fcb233e1b86153f9
John Miclat <jmiclat@ufl.edu>
2023-12-21 23:39:04 -0500

    Initialize auth

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"backend/internal/auth"
 	"backend/internal/server"
 	"fmt"
 )
 
 func main() {
 
+	// initialize the auth package
+	auth.Init()
+
+	// initialize the server
 	server := server.NewServer()
 
 	err := server.ListenAndServe()
