055599b9a8ad7a607a584d1742f26b02726751f3
Tuan Dang <tuan3dang@gmail.com>
2023-12-22 00:56:52 -0500

    Event page layout

---
+++ frontend/src/components/EventGrid.tsx
@@ -0,0 +1,30 @@
+import EventTile from '../components/EventTile';
+
+function EventGrid() {
+  return (
+    <div className="grid h-[900px] w-[900px] grid-cols-3 grid-rows-3 gap-1.5">
+      <EventTile
+        size="large"
+        gridPos="row-start-1 row-end-3 col-start-1 col-end-3"
+      />
+      <EventTile
+        size="medium"
+        gridPos="row-start-1 row-end-2 col-start-3 col-end-4"
+      />
+      <EventTile
+        size="medium"
+        gridPos="row-start-2 row-end-2 col-start-3 col-end-4"
+      />
+      <EventTile
+        size="medium"
+        gridPos="row-start-3 row-end-4 col-start-1 col-end-2"
+      />
+      <EventTile
+        size="long"
+        gridPos="row-start-3 row-end-4 col-start-2 col-end-4"
+      />
+    </div>
+  );
+}
+
+export default EventGrid;
--- frontend/src/pages/Events.tsx
+++ frontend/src/pages/Events.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-
+import React from 'react';
+import EventGrid from '../components/EventGrid';
 function Events() {
   return (
-    <div>
-      event
+    <div className="mx-auto max-w-[1920px] ">
+      <div className="flex flex-col items-center">
+        <EventGrid />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Events
+export default Events;
