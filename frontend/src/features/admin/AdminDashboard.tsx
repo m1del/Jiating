7b8719770756c3785ad79cce95c632c3c233f44c
John Miclat <jmiclat@ufl.edu>
2023-12-22 08:21:23 -0500

    Clean up code

--- frontend/src/features/admin/AdminDashboard.tsx
+++ frontend/src/features/admin/AdminDashboard.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { GoogleLogoutButton } from '../authentication';
+import { UserInfo } from './components';
 
 const AdminDashboard = () => {
     const {authUser, setAuthUser, setIsLoggedin } = useAuth();
@@ -40,24 +41,14 @@ const AdminDashboard = () => {
     return (
       <div className='flex flex-col items-center justify-center bg-gray-100 p-10 rounded-lg shadow-md'>
           <h1 className='text-4xl font-bold text-gray-800 mb-6'>Admin Dashboard</h1>
-          {authUser && (
-              <div className='flex flex-col justify-center items-center bg-white p-4 rounded-lg shadow'>
-                  <img className='w-24 h-24 rounded-full border-2 border-gray-300 m-3'
-                       src={authUser.avatar_url} alt={authUser.name} />
-                  <p className='text-xl text-gray-700'>
-                      Hi, <span className='font-semibold'>{authUser.name}</span>
-                  </p>
-                  <p className='text-md text-gray-600 mt-2'>
-                      Email: <span className='font-semibold'>{authUser.email}</span>
-                  </p>
-              </div>
-          )}
+
+          {authUser && <UserInfo user={authUser} />}
+
           <div className='mt-5'>
               <GoogleLogoutButton />
           </div>
       </div>
   );
-  
 };
 
 export default AdminDashboard;
