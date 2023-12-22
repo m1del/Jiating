fa7f8ab38e82a7eb78ea1fcc22b694b23e537ffa
John Miclat <jmiclat@ufl.edu>
2023-12-21 23:43:43 -0500

    Add auth server, initialize gothic

---
+++ backend/internal/auth/auth.go
@@ -0,0 +1,41 @@
+package auth
+
+import (
+	"log"
+	"os"
+
+	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+)
+
+const (
+	key    = "random string for demo purposes only"
+	MaxAge = 86400 * 30 // 30 days
+	IsProd = false
+)
+
+func Init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	googleClientID := os.Getenv("GOOGLE_CLIENT_ID")
+	googleClientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
+
+	store := sessions.NewCookieStore([]byte(key))
+	store.MaxAge(MaxAge)
+
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true
+	store.Options.Secure = IsProd
+
+	gothic.Store = store
+
+	goth.UseProviders(
+		google.New(googleClientID, googleClientSecret, "http://localhost:3000/auth/google/callback"),
+	)
+}
