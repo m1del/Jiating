1dc29b2dae4de93b0f9aab5cba7e93fa1507455a
John Miclat <jmiclat@ufl.edu>
2023-12-22 06:53:43 -0500

    Add types

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,9 +1,23 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
-const AuthContext = createContext();
+type UserType = {
+  id: string;
+  email: string;
+  name: string;
+  avatar_url: string;
+};
 
-export function AuthProvider(props) {
-  const [authUser, setAuthUser] = useState(null);
+type AuthContextType = {
+  authUser: UserType | null;
+  setAuthUser: (user: UserType | null) => void;
+  isLoggedin: boolean;
+  setIsLoggedin: (loggedIn: boolean) => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: React.PropsWithChildren<{}>) {
+  const [authUser, setAuthUser] = useState<UserType | null>(null);
   const [isLoggedin, setIsLoggedin] = useState(false);
 
   const value = {
@@ -11,15 +25,19 @@ export function AuthProvider(props) {
     setAuthUser,
     isLoggedin,
     setIsLoggedin,
-  }
-  
+  };
+
   return (
     <AuthContext.Provider value={value}>
-      {props.children}
+      {children}
     </AuthContext.Provider>
-  )
+  );
 }
 
-export function useAuth() {
-  return useContext(AuthContext);
+export function useAuth(): AuthContextType {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
 }
