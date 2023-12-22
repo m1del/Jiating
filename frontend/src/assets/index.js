4f18d0a48c4e86e110e3b4d8103235dfac02a947
John Miclat <jmiclat@ufl.edu>
2023-12-22 02:57:43 -0500

    Remove unused assets

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,6 +1,4 @@
 import bwLogo from "./bw-logo.png";
 import logo from "./logo.png";
-import menu from "./menu-bars.svg";
 import teamLogoBW from './team-bw-logo.png';
-import close from "./xmark-solid.svg";
-export { bwLogo, close, logo, menu, teamLogoBW };
+export { bwLogo, logo, teamLogoBW };
