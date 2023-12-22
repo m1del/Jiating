caee1b553c6ae808fe8977ed6b31554aa7932395
John Miclat <jmiclat@ufl.edu>
2023-12-22 07:53:59 -0500

    Made it pretty and display email

--- frontend/src/features/admin/AdminDashboard.tsx
+++ frontend/src/features/admin/AdminDashboard.tsx
@@ -38,18 +38,26 @@ const AdminDashboard = () => {
     
   
     return (
-        <div className='flex flex-col items-center justify-center'>
-            <h1 className='text-3xl text-bold mb-4'>Admin Dashboard</h1>
-            {authUser && (
-                <div className='flex justify-center items-center'>
-                    <p className='text-lg'>hi, <span>{authUser.email}</span></p>
-                    <img className='rounded-full m-5'
-                    src={authUser.avatar_url} alt={authUser.name} />
-                </div>
-            )}
-            <GoogleLogoutButton />
-        </div>
-    );
+      <div className='flex flex-col items-center justify-center bg-gray-100 p-10 rounded-lg shadow-md'>
+          <h1 className='text-4xl font-bold text-gray-800 mb-6'>Admin Dashboard</h1>
+          {authUser && (
+              <div className='flex flex-col justify-center items-center bg-white p-4 rounded-lg shadow'>
+                  <img className='w-24 h-24 rounded-full border-2 border-gray-300 m-3'
+                       src={authUser.avatar_url} alt={authUser.name} />
+                  <p className='text-xl text-gray-700'>
+                      Hi, <span className='font-semibold'>{authUser.name}</span>
+                  </p>
+                  <p className='text-md text-gray-600 mt-2'>
+                      Email: <span className='font-semibold'>{authUser.email}</span>
+                  </p>
+              </div>
+          )}
+          <div className='mt-5'>
+              <GoogleLogoutButton />
+          </div>
+      </div>
+  );
+  
 };
 
 export default AdminDashboard;
