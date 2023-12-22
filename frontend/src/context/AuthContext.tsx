321fbb4c5e24bad92f10b782fab1acf8eb85c06b
John Miclat <jmiclat@ufl.edu>
2023-12-22 04:22:45 -0500

    Implement auth context and provider

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { ReactNode, createContext, useContext, useState } from 'react';
 
-export default function AuthContext() {
+type AuthContextType = {
+  isAuthenticated: boolean;
+  user: any; // TODO: define user type
+  setUser: (user: any) => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState(null);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <AuthContext.Provider value={{ isAuthenticated: !!user, user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+  return context;
+};
