abca92814791638da12e863af2fbb6c60da9be6a
John Miclat <jmiclat@ufl.edu>
2023-12-22 23:55:47 -0500

    Add defined logging package for consistency

---
+++ backend/loggers/loggers.go
@@ -0,0 +1,18 @@
+package loggers
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	InfoLogger  *log.Logger
+	ErrorLogger *log.Logger
+	DebugLogger *log.Logger
+)
+
+func init() {
+	InfoLogger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+	ErrorLogger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	DebugLogger = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime)
+}
