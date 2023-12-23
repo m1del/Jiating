88a25551324891ef727b8a6d93d74e4b8a3d6ffa
John Miclat <jmiclat@ufl.edu>
2023-12-22 23:57:24 -0500

    Modify logger names

--- backend/loggers/loggers.go
+++ backend/loggers/loggers.go
@@ -6,13 +6,13 @@ import (
 )
 
 var (
-	InfoLogger  *log.Logger
-	ErrorLogger *log.Logger
-	DebugLogger *log.Logger
+	Info  *log.Logger
+	Error *log.Logger
+	Debug *log.Logger
 )
 
 func init() {
-	InfoLogger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
-	ErrorLogger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-	DebugLogger = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime)
+	Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+	Error = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Debug = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime)
 }
