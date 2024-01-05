4f394ac40464ffd5936208239a89a924dd399345
John Miclat <jmiclat@ufl.edu>
2024-01-04 20:46:23 -0500

    Move admin stuff to admin dashboard

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
 import FloatBar from '../components/FloatBar';
 import Hero from '../components/Hero';
 import HomeHeading from '../components/HomeHeading';
-import AdminForm from '../features/admin/components/AdminForm';
-import AdminList from '../features/admin/components/AdminList';
 import { GoogleLoginButton } from '../features/authentication';
 
 function Home() {
