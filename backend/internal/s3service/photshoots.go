2baaa74dd17ded01be80cf6a44b8374fe1537f4a
John Miclat <jmiclat@ufl.edu>
2024-01-07 00:46:17 -0500

    Add getter for events in s3

--- backend/internal/s3service/photshoots.go
+++ backend/internal/s3service/photshoots.go
@@ -33,3 +33,26 @@ func (s *service) GetYears() ([]string, error) {
 
 	return years, nil
 }
+
+func (s *service) GetEvents(year string) ([]string, error) {
+	bucket := os.Getenv("S3_BUCKET_NAME")
+	prefix := fmt.Sprintf("photoshoots/%s/", year)
+
+	output, err := s.s3Client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket:    aws.String(bucket),
+		Prefix:    aws.String(prefix),
+		Delimiter: aws.String("/"),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list objects from s3: %v", err)
+	}
+
+	var events []string
+	for _, content := range output.CommonPrefixes {
+		event := strings.TrimPrefix(*content.Prefix, prefix)
+		event = strings.TrimSuffix(event, "/")
+		events = append(events, event)
+	}
+
+	return events, nil
+}
