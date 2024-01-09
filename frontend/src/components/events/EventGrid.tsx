e6fec0b2d26664908ad8a1bb2874c1e34fe1bd40
Tuan Dang <tuan3dang@gmail.com>
2024-01-09 11:23:19 -0500

    Event grid responsive for width over 1280px

--- frontend/src/components/events/EventGrid.tsx
+++ frontend/src/components/events/EventGrid.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import EventTile from './EventTile';
 
 function EventGrid() {
+  //Grid height and width should stay same relatively, 56.25 / 75 = 0.75
   return (
-    <div className="my-48 grid h-[56.25rem] w-[75rem] grid-cols-4 grid-rows-3 gap-1.5">
+    <div className="lg: my-36 grid grid-cols-4 grid-rows-3 gap-1.5 xl:h-[46.875rem] xl:w-[62.5rem] 2xl:h-[56.25rem] 2xl:w-[75rem]">
       <EventTile
         size="large"
         gridPos="row-start-1 row-end-3 col-start-1 col-end-3"
