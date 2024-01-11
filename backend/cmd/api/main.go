c8ab1acab29c7c2c0cff491c52eb7076cd08f982
John Miclat <jmiclat@ufl.edu>
2024-01-10 19:32:25 -0500

    Move auth into Server as a service

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,29 +1,16 @@
 package main
 
 import (
-	"backend/internal/auth"
 	"backend/internal/server"
 	"backend/loggers"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	// load env
-	err := godotenv.Load()
-	if err != nil {
-		loggers.Error.Fatalf("Error loading .env file: %s", err)
-	}
-
-	// initialize the auth package
-	loggers.Info.Println("Initializing auth package...")
-	auth.NewAuth()
-
 	// initialize the server
 	server := server.NewServer()
 
-	err = server.ListenAndServe()
+	err := server.ListenAndServe()
 	if err != nil {
 		loggers.Error.Fatalf("Error starting server: %s", err)
 	}
