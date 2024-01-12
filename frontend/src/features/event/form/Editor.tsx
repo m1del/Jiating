3e2920bb251e03ef863745c9672bdd1120e2cacf
John Miclat <jmiclat@ufl.edu>
2024-01-12 00:05:08 -0500

    Modify toolbar options

--- frontend/src/features/event/form/Editor.tsx
+++ frontend/src/features/event/form/Editor.tsx
@@ -4,8 +4,31 @@ import { useEffect } from "react";
 import { useQuill } from "react-quilljs";
 
 const Editor = () => {
+    const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+
+    ['clean'],                                         // remove formatting
+  ];
+
     const {quill, quillRef, Quill} = useQuill({
-        modules: { blotFormatter: {} }
+        modules: { 
+            toolbar: toolbarOptions,
+            blotFormatter: {} 
+        }
     });
 
     if (Quill && !quill) {
