728858e0d8b75a7f0e09755de8a97a914784adee
John Miclat <jmiclat@ufl.edu>
2024-01-12 06:44:16 -0500

    Temp page for event feature development

--- frontend/src/features/event/form/Editor.tsx
+++ frontend/src/features/event/form/Editor.tsx
@@ -1,7 +1,22 @@
+import Quill from 'quill';
 import BlotFormatter from "quill-blot-formatter";
 import 'quill/dist/quill.snow.css';
 import { useEffect } from "react";
 import { useQuill } from "react-quilljs";
+
+const ImageBlot = Quill.import('formats/image');
+
+class CustomImageBlot extends ImageBlot {
+  static create(value: any) {
+    const node = super.create(value);
+    // Add custom logic to manipulate the image node here
+    node.setAttribute('data-is-display', 'false');
+    return node;
+  }
+}
+
+Quill.register(CustomImageBlot);
+
 
 const Editor = () => {
     const toolbarOptions = [
@@ -40,6 +55,14 @@ const Editor = () => {
     useEffect(() => {
         if (quill) {
             quill.on('text-change', (delta, oldContents)  => {
+                const images = document.querySelectorAll('ql-editor img');
+                images.forEach((img) => {
+                    img.addEventListener('click', () => {
+                        //logic to mark image as display image
+                        // set data IsDisplay to true
+                    })
+                })
+
                 // text is changing
                 console.log('text change')
                 console.log(delta);
